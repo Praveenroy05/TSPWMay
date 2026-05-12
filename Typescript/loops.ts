b14a31fa240014360992530b6ed941c4d7c1a47d
Praveen Roy <roykumar0593@gmail.com>
2026-05-12 07:58:26 +0530

    added loops

---
+++ Typescript/loops.ts
@@ -0,0 +1,24 @@
+// Loops - Will execute the same block {...} of code multiple times until the 
+// condition is false.
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+console.log("********************************")
+
+// for(let i=1; i<=5; i++){
+//     console.log(i)
+// }
+
+
+// 1. for loop
+      // a. for loop - Traditional for loop - When we know that how many times we have to run the iteration
+      // b. for of loop - Loop through the values of an iterable object (like an array or string).
+      // c. for in loop - Loop through the properties of an object. {name: "john"}
+// 2. while loop - When we don't know that how many times we have to run the iteration
+// 3. do while loop - Similar to while loop but it will execute the block of code at 
+// least once even if the condition is false. 
+
--- Typescript/statements.ts
+++ Typescript/statements.ts
@@ -5,7 +5,8 @@
 // 2. if else statement - This will handle both positive and negative scenario
 // 3. if else if statement - This will handle multiple scenarios based on different conditions
 // 4. switch statement
-// 5. ternary operator
+// 5. ternary operator - if else statement in a single line of code.
+
 
 
 // 1. if statement
@@ -79,4 +80,63 @@ else if (browser == "safari"){ // "edge" == "safari"
 }
 else{
     console.log("Please provide the correct browser name");
+}
+
+
+// 4. switch statement
+// Syntax:
+
+/*
+
+switch(expression){
+    case value1:
+        // block of code to be executed if expression == value1
+        break;
+    case value2:
+        // block of code to be executed if expression == value2
+        break;
+    default:
+        // block of code to be executed if none of the above cases match
+
+}
+*/
+
+
+let browserName = "edge"
+
+switch(browserName){ // switch("chrome")
+    case "chrome":
+        console.log("Launch the chrome browser");
+        break;
+    case "firefox":
+        console.log("Launch the firefox browser");
+        break;
+    case "safari":
+        console.log("Launch the safari browser");
+        break;
+    default:
+        console.log("Please provide the correct browser name- switch");
+}
+
+// 5. Ternary operator(? :) - This is a shorthand way of writing an if else statement.
+
+// Syntax:
+
+// condition ? "value if true" : "value if false"
+
+// 1. condition ?
+// 2. The value if the condition is true
+// 3. The value if the condition is false
+
+
+let age2 = 10;
+
+
+(age2 >=18)? console.log("You are eligible to vote- TO") : console.log("You are not eligible to vote - TO");
+
+if(age2 >=18){
+    console.log("You are eligible to vote");
+}
+else{
+    console.log("You are not eligible to vote");
 }
