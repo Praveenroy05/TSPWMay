9cca73a88399d4c833b2749b96277dec648bc88c
Praveen Roy <roykumar0593@gmail.com>
2026-05-13 08:04:01 +0530

    added loops

---
+++ Typescript/functions.ts
@@ -0,0 +1,17 @@
+// Functions - It is a set of instruction that perform a specific task
+
+function login(){
+    // fill the username
+    // fill the password
+    // click on login button
+}
+
+// login()
+
+
+// 100 Test case  - 300 lines of code  - 195 lines of code saved
+
+// Duplication - to solve the problem of dup
+// Maintenance - 100 to fix the problem - Saved 99 steps for maintenance
+// Reusability - We can use the same function in multiple test cases
+
--- Typescript/loops.ts
+++ Typescript/loops.ts
@@ -22,3 +22,87 @@ console.log("********************************")
 // 3. do while loop - Similar to while loop but it will execute the block of code at 
 // least once even if the condition is false. 
 
+
+// 1. for loop
+// Syntax:
+/*
+
+for(initialization; condition; increment/decrement)
+{
+   // block of code to be executed
+}
+
+initialization - Initialise the value of the variable to start the execution. Ex - let i=0
+condition - Condition to check if the loop should continue or not. Ex: - i<5
+increment/decrement - Increase or decrease the value of the variable. i++/i--
+
+
+*/
+
+let a = 10 // initialisation
+let b // declaration
+
+for(let i=1; i<=5; i++){ // 6<=5 -false
+      console.log(i)    // 1 2 3 4 5 
+      if(i == 3){
+            break
+      }
+}
+
+// break - Which terminate the loop when the specified condition is met
+
+
+console.log("***************************")
+
+// 2. While loop
+// Syntax:
+
+/*
+
+initialisation - let j  = 1
+
+while(condition){
+  // block of code
+  increment/decrement
+}
+
+*/
+
+// Print 10 to 1 using while loop
+
+let j = 10
+
+while(j>10){ // j>=1 
+    console.log(j) // 10 9 8 ......1
+    j-- // 0
+}
+
+
+console.log("****************************")
+
+
+// 3. do while loop
+
+//Syntax:
+
+/*
+
+initialiazation
+
+do{
+  // block of code
+  increment/decrement
+}
+while(condition);
+
+*/
+
+let k =10
+
+do{
+      console.log(k) // 10
+      k-- // 9
+}while(k>10) // 9>10
+
+
+
