0565d36a0b8d430732c144576cee45d8b7a0992f
Praveen Roy <roykumar0593@gmail.com>
2026-05-21 08:09:28 +0530

    added github

---
+++ Typescript/github.ts
@@ -0,0 +1,56 @@
+// GIthub/Gitlab/Bitbucket - Version control tool
+
+// To work with github/gitlab/bitbucket, we need to have "git" installed in our local system
+// We can download git from https://git-scm.com/install/windows and install in our local
+
+// 1. Account - We need to create account on the version control tool
+// 2. Repository - A repository is a storage space where you host/store the 
+// different versions of code.
+
+// You are the first employee to start test automation - NO REPO AVAILABLE
+
+// 0. Create a repository on version control tool
+// 1. Create a project and write the test cases in your local system
+// 2. git init - Initialize a new git repo inside your local system
+// 3. git add . - Add all the files to your local git area
+// 4. git commit -m "first commit" - Commit the changes to the local got with a message
+// 5. git branch -M master - Create a new branch as "master"
+// 6. git branch - List all the branches that in your repository.
+// 7. git remote add origin https://github.com/Praveenroy05/PWTest.git - Add a remote
+// repo to your local repo and link it with the remote repo
+// 8. git push -u origin master - Push the commited changes from the local "master"
+// branch to the remote master branch
+
+// 9. git checkout -b branchName - Create a new branch going forward for all the new task - 
+// 10 . repeat # 5
+// 11. Complete writing the test case and follow the steps from #3, #4 and #8
+// 12. You have to open a PR (Pull Request) in the remote repo
+
+// Once you again start working on the new scenarios:
+// 13. <git checkout master> - Go to the master branch and pull all the latest changes inside your local system
+// go to the master branch in you local system and pull all the recent changes
+// <git pull>
+
+// 14. Repeat always the steps from #9 to #13
+
+
+// If the repository is already available
+
+// 1. git clone repo-url - clone the respository  in your local sytem
+// 2. npm install - Install all the dependencies
+// 3. git checkout -b branchName - Create a new branch going forward for all the new task - 
+// 4. Write the test cases in your local system and save the files
+// 5. git status - Validate all the files that you have changes
+// 6. git add . - Add all the files to your local git area
+// 7. git commit -m "first commit" - Commit the changes to the local got with a message
+   // 7.1. git checkout master - Switch to the master branch
+   // 7.2. git pull - Pull all the latest changes from your remote master branch to to the local master branch
+   // 7.3. git checkout branchName - Switch back to your feature branch
+   // 7.4. git merge master - Merge master branch to "DP" branch  
+// 8. git push -u origin dp - Push the commited changes from the local "dp"
+// branch to the remote master branch
+// 9 . Go to Github/Gitlab/Bitbucket and create a pull request (PR) to merge your changes 
+// to the master branch.
+
+
+// #3 - #9
--- Typescript/strings.ts
+++ Typescript/strings.ts
@@ -64,6 +64,59 @@
     console.log(str3.substring(1))
     console.log(str3.substring(2, 9))
     console.log(str3.substring(9, 2))
+
+
+    // 9. startsWith(string) - it check if the string is starting with the specified char or string
+    // and return the result in the form of boolean.
+
+    console.log(str3.startsWith("Welcome"))
+
+    // 10. endsWith(string) - it check if the string is ending with the specified char or string
+    // and return the result in the form of boolean.
+
+
+    // 11. toUpperCase() - It converts the string to upper case letter and return a new string
+    console.log(str3)
+    console.log(str3.toUpperCase())
+
+    // 12. toLowerCase() - It converts the string to lower case letter and return a new string
+    console.log(str3.toLowerCase())
+
+    let str4 = "   Electronic   "
+    
+    // 13. trim() -  it removes the leading and trailing whitespace from a string and return a new string
+    
+    console.log("Electronic" == str4.trim());
+
+    // 14. trimStart() - it removes the leading whitespace from a string and return a new string
+    console.log(str4.length);
+    console.log(str4.trimStart().length)
+
+    // 15. trimEnd() - it removes the trailing whitespace from a string and return an new string
+    console.log(str4.trimEnd().length)
+
+    // 16. split(seperator) - Splits the string into an array of substring based on the specified separator
+
+    let str5 = "Apple banana mango orange" 
+    console.log(str5.split(" ")); // ["Apple", "banana", "mango", "orange"]
+
+    let email = "praveen@qamitra.com"
+    let domain = email.split("@")
+    console.log(domain[1].split(".")[0])
+
+    let dname = domain[1].split(".")
+    console.log(dname[0]);
+
+
+    //17. replace(old, new) - it will replace the old char to new char for the first occurances only
+
+    console.log(email.replace("a", "@"));
+
+    // 18. replaceAll(old, new)
+    console.log(email.replaceAll("a", "@"));
+
+
+    
     
     
     
